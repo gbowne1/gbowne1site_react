ac8b0b4d328c5667629dce39a3a5c7640314437b
Atharva Shinde <shindeathrv02@gmail.com>
2023-10-28 19:44:01 +0530

    Fixed Contact Page

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -45,26 +45,29 @@ class Contact extends React.Component {
                 variant="outlined"
               />{" "}
               {/* Using the TextField component */}
-              <Card variant="outlined">
-                {" "}
-                {/* Using the Card component */}
-                <CardHeader title="Contact Information" />
-                <CardContent>
-                  <p>Name: John Doe</p>
-                  <p>Email: john.doe@example.com</p>
-                  <p>Phone: 123-456-7890</p>
-                </CardContent>
-              </Card>
-              <Paper variant="outlined">
-                {" "}
-                {/* Using the Paper component */}
-                <p>Office Address:</p>
-                <p>123 Main St.</p>
-                <p>Anytown, USA 12345</p>
-              </Paper>
               <button className="Contact-submit-button">Submit</button>
             </form>
           </div>
+        </div>
+        <div class= "Contact-info">
+          <Card variant="outlined">
+          {" "}
+          {/* Using the Card component */}
+          <CardHeader title="Contact Information" />
+          <CardContent>
+            <p>Name: John Doe</p>
+            <p>Email: john.doe@example.com</p>
+            <p>Phone: 123-456-7890</p>
+          </CardContent>
+        </Card>
+        <br />
+        <Paper variant="outlined">
+          {" "}
+          {/* Using the Paper component */}
+          <p>Office Address:</p>
+          <p>123 Main St.</p>
+          <p>Anytown, USA 12345</p>
+        </Paper>
         </div>
         {/* Using the Footer component */}
       </div>
