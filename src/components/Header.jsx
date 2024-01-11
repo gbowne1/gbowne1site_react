45481e4983acb48b3eccfd6d4cc6bc0bd1cdef48
Jigin Jayaprakash <145117767+JiginJayaprakash@users.noreply.github.com>
2024-01-11 13:31:28 +0000

    Migrate2Function

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,7 @@
-import React from "react";
 import "../assets/styles/Header.css";
 
-class Header extends React.Component {
-  render() {
-    return <header></header>;
-  }
-}
+const Header = () => {
+  return <header></header>;
+};
 
 export default Header;
--- src/components/NotFound.jsx
+++ src/components/NotFound.jsx
@@ -1,19 +1,16 @@
-import React from "react";
 import "../assets/styles/NotFound.css";
 import notFound from "../assets/images/sitechecker-404.png";
 
-class NotFound extends React.Component {
-  render() {
-    return (
-      <div className="container">
-        <p>Oops! The page you were looking for doesn&apos;t exist.</p>
-        <a href="/" className="button">
-          Go back to the homepage
-        </a>
-        <img src={notFound} />
-      </div>
-    );
-  }
-}
+const NotFound = () => {
+  return (
+    <div className="container">
+      <p>Oops! The page you were looking for doesn&apos;t exist.</p>
+      <a href="/" className="button">
+        Go back to the homepage
+      </a>
+      <img src={notFound} />
+    </div>
+  );
+};
 
 export default NotFound;
