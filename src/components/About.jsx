76e83b9fe456cdae0e5c7476396f756a59abdf0a
Greg Bowne <gbowne144@gmail.com>
2024-01-16 16:14:27 -0800

    Updated files by running lint and format

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -12,11 +12,11 @@ const About = () => {
           <img src={profilePic} alt="Headshot of Gregory Bowne" />
           <p>
             Hi there! my name is Gregory Bowne aka gbowne1. I am a 44 year old
-            small business owner in the manufacturing sector. I own a machine and
-            fabrication / engineering workshop with about 250 employees as of
-            4/30/2021. I also do manufacturing consulting as well as ERP & MRP
-            software consutling for the major ERP and MRP software packages for
-            regional manufacturing compaines and small machine shops.
+            small business owner in the manufacturing sector. I own a machine
+            and fabrication / engineering workshop with about 250 employees as
+            of 4/30/2021. I also do manufacturing consulting as well as ERP &
+            MRP software consutling for the major ERP and MRP software packages
+            for regional manufacturing compaines and small machine shops.
           </p>
         </div>
       </article>
@@ -26,14 +26,19 @@ const About = () => {
           <h3>Businesses</h3>
           <ul>
             <li>
-              <strong>Workshop Ownership:</strong><br />
-              Owns a machine and fabrication/engineering workshop with ~250 employees (as of 4/30/2021).
+              <strong>Workshop Ownership:</strong>
+              <br />
+              Owns a machine and fabrication/engineering workshop with ~250
+              employees (as of 4/30/2021).
             </li>
 
             <li>
-              <strong>Consulting:</strong><br />
-              Provides manufacturing and ERP/MRP software consulting for regional manufacturing companies and small machine shops. Works with E2, JobBoss, Odoo, Microsoft Dynamics, and others.
-              </li>
+              <strong>Consulting:</strong>
+              <br />
+              Provides manufacturing and ERP/MRP software consulting for
+              regional manufacturing companies and small machine shops. Works
+              with E2, JobBoss, Odoo, Microsoft Dynamics, and others.
+            </li>
           </ul>
         </div>
       </article>
@@ -41,92 +46,152 @@ const About = () => {
         <div className="box">
           <h3>Careers / Jobs</h3>
           <ul>
-            <li><strong>Workshop and Consulting:</strong><br />
-              Machine and fabrication/engineering workshop owner with ~250 employees. Provides manufacturing and ERP/MRP software consulting for regional companies.
-              </li>
+            <li>
+              <strong>Workshop and Consulting:</strong>
+              <br />
+              Machine and fabrication/engineering workshop owner with ~250
+              employees. Provides manufacturing and ERP/MRP software consulting
+              for regional companies.
+            </li>
 
             <li>
-              <strong>Programming:</strong><br />
+              <strong>Programming:</strong>
+              <br />
               Codes as a hobby, working on personal projects.
             </li>
 
             <li>
-              <strong>Mechanical CAD, CAM, CNC:</strong><br />
-              Experienced in Mechanical CAD, CAM, and CNC Programming. Former Autodesk Certified Instructor. Uses FreeCAD and KiCAD, not Autodesk products.
+              <strong>Mechanical CAD, CAM, CNC:</strong>
+              <br />
+              Experienced in Mechanical CAD, CAM, and CNC Programming. Former
+              Autodesk Certified Instructor. Uses FreeCAD and KiCAD, not
+              Autodesk products.
             </li>
 
             <li>
-              <strong>Software Experience:</strong><br />
-              Core experience in AutoCAD 2002-2009, switched to Fusion 360 in mid-2013.
+              <strong>Software Experience:</strong>
+              <br />
+              Core experience in AutoCAD 2002-2009, switched to Fusion 360 in
+              mid-2013.
             </li>
 
             <li>
-              <strong>CNC Programming:</strong><br />
-              Occasionally does CNC programming for 3 and 4 axes milling and lathes for a fee.
+              <strong>CNC Programming:</strong>
+              <br />
+              Occasionally does CNC programming for 3 and 4 axes milling and
+              lathes for a fee.
             </li>
 
             <li>
-              <strong>Microsoft Office Specialist (MOS):</strong><br />
-              Certified for Microsoft Office products through Office 2010 on Windows.
+              <strong>Microsoft Office Specialist (MOS):</strong>
+              <br />
+              Certified for Microsoft Office products through Office 2010 on
+              Windows.
             </li>
           </ul>
         </div>
       </article>
-    
+
       <article className="container-item About-hobbies">
         <div className="box">
           <h3>Hobbies</h3>
           <ul>
             <li>
-              <strong>Crafting:</strong><br/>
+              <strong>Crafting:</strong>
+              <br />
               Various crafts as a hobby.
             </li>
 
-            <li><strong>Tech and Programming:</strong><br/>
-            Building and programming PCs.<br/>
-            IT work for home and business.<br/>
+            <li>
+              <strong>Tech and Programming:</strong>
+              <br />
+              Building and programming PCs.
+              <br />
+              IT work for home and business.
+              <br />
             </li>
 
             <li>
-              <strong>Metalworking/Machining:</strong><br />
-              Collecting vintage machine tools.<br/>
-              Metalworking as a main hobby.<br />
+              <strong>Metalworking/Machining:</strong>
+              <br />
+              Collecting vintage machine tools.
+              <br />
+              Metalworking as a main hobby.
+              <br />
             </li>
 
             <li>
-              <strong>Radio and Electronics:</strong><br />
-              Designing computers in the 8, 16, and early 32bit realm.<br />
-              Shortwave radio listening.<br />
-              Building antennas.<br />
+              <strong>Radio and Electronics:</strong>
+              <br />
+              Designing computers in the 8, 16, and early 32bit realm.
+              <br />
+              Shortwave radio listening.
+              <br />
+              Building antennas.
+              <br />
             </li>
 
             <li>
-              <strong>DXing and Communication:</strong><br />
-              Active participation in local radio DX parties.<br />
-              Long-distance AM (Medium Wave) reception.<br />
-              Active on GMRS and FRS.<br />
+              <strong>DXing and Communication:</strong>
+              <br />
+              Active participation in local radio DX parties.
+              <br />
+              Long-distance AM (Medium Wave) reception.
+              <br />
+              Active on GMRS and FRS.
+              <br />
             </li>
           </ul>
         </div>
-        </article>
-        <article className="container-item About-computer-specs">
+      </article>
+      <article className="container-item About-computer-specs">
         <div className="box">
           <h3>Computer Specs</h3>
           <ul>
-            <li><strong>Case:</strong> CoolerMaster HAF 932</li>
-            <li><strong>Monitor:</strong> ACER @ 1920x1080 60Hz</li>
-            <li><strong>Keyboard:</strong> Logitech K520 with Logitech Unifying Receiver</li>
-            <li><strong>Mouse:</strong> Logitech M325 (Gray)</li>
-            <li><strong>Graphics:</strong> NVIDIA GF100GL [Quadro 4000] 2gb</li>
-            <li><strong>Motherboard:</strong> ASUSTek P8Z68-V PRO GEN3 v: Rev 1.xx</li>
-            <li><strong>RAM:</strong> 32GB</li>
-            <li><strong>Webcam:</strong> Logitech Webcam Pro 9000</li>
-            <li><strong>CPU:</strong> Intel Core i7-2600</li>
-            <li><strong>Printer:</strong> HP OfficeJet 8710</li>
-            <li><strong>Sound:</strong> Creative Labs X-Fi with EMU20k1</li>
-            <li><strong>Total Storage:</strong> 6.5TB</li>
-            <li><strong>Power Supply:</strong> eVGA 750w</li>
-            <li><strong>Optical Media:</strong> 48x Generic iATAPI DVD-RW (not enumerated properly)</li>
+            <li>
+              <strong>Case:</strong> CoolerMaster HAF 932
+            </li>
+            <li>
+              <strong>Monitor:</strong> ACER @ 1920x1080 60Hz
+            </li>
+            <li>
+              <strong>Keyboard:</strong> Logitech K520 with Logitech Unifying
+              Receiver
+            </li>
+            <li>
+              <strong>Mouse:</strong> Logitech M325 (Gray)
+            </li>
+            <li>
+              <strong>Graphics:</strong> NVIDIA GF100GL [Quadro 4000] 2gb
+            </li>
+            <li>
+              <strong>Motherboard:</strong> ASUSTek P8Z68-V PRO GEN3 v: Rev 1.xx
+            </li>
+            <li>
+              <strong>RAM:</strong> 32GB
+            </li>
+            <li>
+              <strong>Webcam:</strong> Logitech Webcam Pro 9000
+            </li>
+            <li>
+              <strong>CPU:</strong> Intel Core i7-2600
+            </li>
+            <li>
+              <strong>Printer:</strong> HP OfficeJet 8710
+            </li>
+            <li>
+              <strong>Sound:</strong> Creative Labs X-Fi with EMU20k1
+            </li>
+            <li>
+              <strong>Total Storage:</strong> 6.5TB
+            </li>
+            <li>
+              <strong>Power Supply:</strong> eVGA 750w
+            </li>
+            <li>
+              <strong>Optical Media:</strong> 48x Generic iATAPI DVD-RW (not
+              enumerated properly)
+            </li>
           </ul>
         </div>
       </article>
